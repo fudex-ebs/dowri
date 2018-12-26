b9085ac90ce979263517f4c933e17b8f21d53f06
moosa bukhamsin <moosabukhamsin@gmail.com>
2018-12-26 11:35:53 +0300

    front_end_modification

--- resources/views/admin/footer.blade.php
+++ resources/views/admin/footer.blade.php
@@ -3,7 +3,7 @@
     <div class="m-stack m-stack--flex-tablet-and-mobile m-stack--ver m-stack--desktop">
       <div class="m-stack__item m-stack__item--left m-stack__item--middle m-stack__item--last">
         <span class="m-footer__copyright">
-          Dowri 2018
+          Doari 2018
         </span>
       </div>
     </div>
